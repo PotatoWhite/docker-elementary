df6827b3244116147b55339bd834d3fbfce525d2
Potato White <bravopotato@gmail.com>
2019-09-18 20:11:05 +0900

    2tier move to preschool

--- demo-preschool-2tier/src/main/java/me/potato/demopreschool2tier/SampleRepository.java
+++
@@ -1,7 +0,0 @@
-package me.potato.demopreschool2tier;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleRepository extends JpaRepository<Sample, Long> {
-
-}
